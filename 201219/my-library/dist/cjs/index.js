fb610b98d79ad03e2d2c147538d8319867a7008e
shch <shch1289@gmail.com>
2020-12-19 22:54:39 +0800

    feat: 更新rollup库案例

---
+++ 201219/my-library/dist/cjs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function hello() {
+    console.log("hello");
+  }
+
+function goodbye() {
+    console.log("goodbye");
+  }
+
+exports.goodbye = goodbye;
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
---
+++ 201219/my-library/dist/esm/index.js
@@ -0,0 +1,10 @@
+function hello() {
+    console.log("hello");
+  }
+
+function goodbye() {
+    console.log("goodbye");
+  }
+
+export { goodbye, hello };
+//# sourceMappingURL=index.js.map
